a53798ffba2c17a05852219d1eb42313ede88edf
Mohamed Amine Ben Salah <contact@damine.wtf>
2019-06-25 17:53:02 -0300

    adding fetch and display repositories

--- app.js
+++ app.js
@@ -14,14 +14,15 @@ searchUser.addEventListener('keyup', (e) => {
         github.getUser(userText).then(data => {
             if(data.profile.message === 'Not Found') {
                 // show alert
-
+                ui.showAlert('User Not Found','alert alert-danger')
             } else {
                 // show profile
                 ui.showProfile(data.profile);
+                ui.showRepos(data.repos);
             }
         })
     } else {
         // clear profile
-
+        ui.clearProfile();
     }
 })
--- github.js
+++ github.js
@@ -2,15 +2,22 @@ class Github {
     constructor(){
         this.client_id = '337c95a2d62a4bedbb8b';
         this.client_secret = 'f460c6fcd7fc76a669a6c1db58365d80de47ed20';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
+        
     }
 
     async getUser(user){
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
         const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
 
         return {
-            profile
+            profile,
+            repos
         }
     }
 }
